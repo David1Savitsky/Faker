b35c9d2e41a8678279d8b9f3009a0cc82e7d5fd9
David1Savitsky <omgprogress24@gmail.com>
2022-09-30 19:19:42 +0300

    Implement faker create method

--- Faker/Faker.cs
+++ Faker/Faker.cs
@@ -1,18 +1,43 @@
 ﻿using System;
+using System.Reflection;
+using Faker.Creator;
+using Faker.Generator;
+using Faker.Value.Generators;
 
 namespace Faker
 {
     public class Faker : IFaker
     {
+
+        private GeneratorContext _context;
+        private DtoCreator _dtoCreator;
+        private ValueGenerator _valueGenerator;
+        
+        public Faker()
+        {
+            _context = new GeneratorContext(new Random(), this);
+            _dtoCreator = new DtoCreator();
+            _valueGenerator = new ValueGenerator();
+
+        }
         public T Create<T>()
         {
-            return (T) Create(typeof(T));
+            return (T)Create(typeof(T));
         }
         
         private object Create(Type t)
         {
-            //TODO
-            throw new NotImplementedException();
+            object obj;
+            if (_valueGenerator.CanGenerate(t))
+            {
+                obj = _valueGenerator.Generate(t,_context);
+            }
+            else
+            {
+                obj = _dtoCreator.Create(t, _context);
+            }
+
+            return obj;
         }
     }
 }
