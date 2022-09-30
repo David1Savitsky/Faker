c974452379e23489f7614d64b4710d48d9460ba8
David1Savitsky <omgprogress24@gmail.com>
2022-09-30 19:21:09 +0300

    Create ValueGenerator for invoking IValueGenerators implementations

---
+++ Faker/Value.Generators/ValueGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Faker.Exceptions;
+using Faker.Generator;
+
+namespace Faker.Value.Generators;
+
+public class ValueGenerator : IValueGenerator
+{
+    private IList<IValueGenerator> _generators;
+
+    public ValueGenerator()
+    {
+        _generators = new GeneratorsList()._generators;
+    }
+
+    public object Generate(Type typeToGenerate, GeneratorContext context)
+    {
+        foreach (var generator in _generators)
+        {
+            if (generator.CanGenerate(typeToGenerate))
+            {
+                return generator.Generate(typeToGenerate, context);
+            }
+        }
+
+        throw new PrimitiveGenerationException();
+    }
+
+    public bool CanGenerate(Type type)
+    {
+        for (int i = 0; i < _generators.Count; i++)
+        {
+            if (_generators[i].CanGenerate(type))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
