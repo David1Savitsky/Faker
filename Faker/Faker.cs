292da24922df7f8b1704de7ed01a0c7cf4a91142
David1Savitsky <omgprogress24@gmail.com>
2022-10-01 15:03:10 +0300

    Implement object initialization using constructor

--- Faker/Faker.cs
+++ Faker/Faker.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Reflection;
-using Faker.Creator;
 using Faker.Generator;
 using Faker.Value.Generators;
 
@@ -10,13 +9,11 @@ namespace Faker
     {
 
         private GeneratorContext _context;
-        private DtoCreator _dtoCreator;
         private ValueGenerator _valueGenerator;
         
         public Faker()
         {
             _context = new GeneratorContext(new Random(), this);
-            _dtoCreator = new DtoCreator();
             _valueGenerator = new ValueGenerator();
 
         }
@@ -34,10 +31,53 @@ namespace Faker
             }
             else
             {
-                obj = _dtoCreator.Create(t, _context);
+                ConstructorInfo constructorInfo = GetConstructorWithMaxCountOfParams(t);
+                var paramValues = new object[constructorInfo.GetParameters().Length];
+                for (int i = 0; i < paramValues.Length; i++)
+                {
+                    if (_valueGenerator.CanGenerate(constructorInfo.GetParameters()[i].ParameterType))
+                    {
+                        paramValues[i] = _valueGenerator.Generate(constructorInfo.GetParameters()[i].ParameterType, _context);
+                    }
+                    else
+                    {
+                        paramValues[i] = Create(constructorInfo.GetParameters()[i].ParameterType);
+                    }
+                }
+                obj = constructorInfo.Invoke(paramValues);
+                
+                /*FieldInfo[] fields = t.GetFields();
+                foreach (var field in fields)
+                {
+                    if (_valueGenerator.CanGenerate(field.FieldType))
+                    {
+                        field.SetValue(obj, _valueGenerator.Generate(field.FieldType, _context));
+                    }
+                    else
+                    {
+                        field.SetValue(obj, Create(field.FieldType));
+                    }
+                }*/
+            }
+            return obj;
+        }
+
+        private ConstructorInfo GetConstructorWithMaxCountOfParams(Type type)
+        {
+            ConstructorInfo[] constructors = type.GetConstructors();
+
+            if (constructors.Length == 1)
+                return constructors[0];
+
+            int indexOfConstructorWithMaxCountOfparams = 0;
+
+            for (int i = 1; i < constructors.Length; i++)
+            {
+                if (constructors[i - 1].GetParameters().Length < constructors[i].GetParameters().Length)
+                    indexOfConstructorWithMaxCountOfparams = i;
             }
 
-            return obj;
+            return constructors[indexOfConstructorWithMaxCountOfparams];
         }
     }
 }
