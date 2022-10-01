f29d43dd6c5da00139d3ab666f053aa553062f86
David1Savitsky <omgprogress24@gmail.com>
2022-10-01 15:02:29 +0300

    Implement list Generator

---
+++ Faker/Value.Generators/Primitive/ListGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using Faker.Generator;
+
+namespace Faker.Value.Generators
+{
+    public class ListGenerator : IValueGenerator
+    {
+        public object Generate(Type typeToGenerate, GeneratorContext context)
+        {
+            var genericTypeArgument = typeToGenerate.GenericTypeArguments[0];
+            var listObject = Activator.CreateInstance(typeToGenerate);
+
+            var length = context.Random.Next(10);
+            
+            var executeMethod = context.Faker
+                .GetType()
+                .GetMethod("Create")?
+                .MakeGenericMethod(genericTypeArgument);
+
+            for (int i = 0; i < length; i++)
+            {
+                var obj = new []{
+                    executeMethod?.Invoke(context.Faker, new object[]{})
+                };
+                
+                typeToGenerate.GetMethod("Add")?.Invoke(listObject, obj);
+            }
+
+            return listObject;
+        }
+
+        public bool CanGenerate(Type type)
+        {
+            var interfaces = type.GetInterfaces();
+            return interfaces.Contains(typeof(IList)) && type.IsGenericType;
+        }
+    }
+}
